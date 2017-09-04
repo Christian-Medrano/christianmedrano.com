9b8d7f89a521c7b151bcdede67ea3a52ca12bea0
Christian Medrano <christianmikhael@yahoo.com>
2017-09-04 11:10:59 -0700

    added behance badge

--- portfolio.php
+++ portfolio.php
@@ -3,47 +3,46 @@
 		<h2>Portfolio</h2>
 		<img id="close-portfolio" class="btn-close" src="img/icons/close.svg" alt="exit">
 	</div>
-	<div id="projects">
-		
-		
-		<div class="project">
-			<div class="cover">
-				<img src="https://www.webpagefx.com/blog/images/cdn.designinstruct.com/files/542-free-branding-identity-mockups/29-branding-identity-mock-up-vol-8-full.jpg" alt="">
-			</div>
-			<div class="project-info">
-				<h3>Project Name</h3>
-				<h4>Branding / Photography</h4>
-			</div>
-		</div>
-		<?php 
-			$postsURL = 'https://api.behance.net/v2/users/christianmedrano/projects?client_id=nJKyMss8eOnHmuOOFVmfSGi9QwyDCZNy';
-			$data = json_decode(file_get_contents($postsURL));
-			$coverSize = "404";
 
-			$i = 0;
+	<div id="projects-container">
+		<div id="projects">
+			<?php 
+				$postsURL = 'https://api.behance.net/v2/users/christianmedrano/projects?client_id=nJKyMss8eOnHmuOOFVmfSGi9QwyDCZNy';
+				$data = json_decode(file_get_contents($postsURL));
+				$coverSize = "404";
 
-			if (isset($data)) {
-				foreach ($data->projects as $project){
-					echo '<div class="project">';
+				$i = 0;
 
-					echo '<div class="cover">';
-					echo '<img src="'.$project->covers->$coverSize.'" alt="">';
-					echo '</div>';
+				if (isset($data)) {
+					foreach ($data->projects as $project){
+						
+						echo '<div class="project">';
+						echo '<a href="'.$project->url.'" target="_blank">';
 
-					echo '<div class="project-info">';
-					echo '<h3>'.$project->name.'</h3>';
-					echo '<ul>';
-					foreach ($project->fields as $field) {
-						echo '<li><h4>'.$field.'</h4></li>';
+						echo '<div class="cover">';
+						echo '<img src="'.$project->covers->$coverSize.'" alt="">';
+						echo '</div>';
+
+						echo '<div class="project-info">';
+						echo '<h3>'.$project->name.'</h3>';
+						echo '<ul>';
+						foreach ($project->fields as $field) {
+							echo '<li><h4>'.$field.'</h4></li>';
+						}
+						echo '</ul>';
+						echo '</div>';
+
+						echo '</a>';
+						echo '</div>';
+						
 					}
-					echo '</ul>';
-					echo '</div>';
-
-					echo '</div>';
+				} else {
+					echo '<p>Could not retrieve projects.</p>';
 				}
-			} else {
-				echo '<p>Could not retrieve projects.</p>';
-			}
-		?>
+			?>
+		</div>
+		<div id="behance-badge">
+			<img src="img/icons/powered-by-behance.svg" alt="">
+		</div>
 	 </div>
 </div>
