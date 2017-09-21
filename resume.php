4cd76089b375505d05781ba8288653b80c712c2a
Christian Medrano <christianmikhael@yahoo.com>
2017-09-20 22:50:58 -0700

    Created Grid and Started Resume

--- resume.php
+++ resume.php
@@ -1,3 +1,35 @@
 <div class="window">
-	
+
+	<div class="resume-download">
+		<div class="row">
+			<div class="col-12">
+				<button>
+					Download my full résumé
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="hr py-3"></div>
+
+	<div class="resume-content">
+		<div class="row">
+
+			<div class="col-12 col-mb-12">
+				<h3 class="text-right"><small>Experience</small></h3>
+			</div>
+
+			<div class="col-6 col-mb-12">
+				<h2>Graphic Design Intern</h2>
+			</div>
+			<div class="col-6 col-mb-12">
+				half width column
+			</div>
+
+			<div class="col-12 col-mb-12">
+				<h3 class="text-right"><small>Education</small></h3>
+			</div>
+		</div>
+	</div>
+
 </div>
